5f78a56489cf9d1ad0095f9becb00a5be33ff195
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-19 07:19:09 +0000

    another instance inserted

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -2,7 +2,8 @@ INSERT INTO users (name, email, password)
 VALUES ('Jeffrey','shaoyuhao970909@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Aaron','Aaron12345@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Stephan','Stephan2333@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-('Serena','Serena88888@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+('Serena','Serena88888@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('alvin', 'Alvin1234567@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
 
 
 
