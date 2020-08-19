23c2c2acb0d074afac1d18fa19a907e44b1a6412
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-19 07:11:20 +0000

    new folder created with a sql file for inserting datas into four tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password) 
+VALUES ('Jeffrey','shaoyuhao970909@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Aaron','Aaron12345@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Stephan','Stephan2333@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Serena','Serena88888@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+
+
